d2bd66e7b25a3436f05ce3385e133e37778b4db5
AnirudhMohanKumar <anirudhmkumar@gmail.com>
2020-05-02 12:58:34 +0530

    Service added

--- web/src/main/java/com/web/WebApplication.java
+++ web/src/main/java/com/web/WebApplication.java
@@ -1,13 +1,37 @@
 package com.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class WebApplication {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${app.url}")
+	private String appUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String getFromApp() {
+		
+		return restTemplate.getForObject(appUrl, String.class);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
